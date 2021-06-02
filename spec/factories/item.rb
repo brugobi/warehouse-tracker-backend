594e14e45de1c85cf9f730eb969cd4a5be7fa081
brugobi <brgobbi@hotmail.com>
2021-06-02 13:52:26 -0400

    add rubocop-faker

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Name.name }
     code { Faker::Code.asin }
-    ideal_quantity { Faker::Number.between(1, 10) }
-    current_quantity { Faker::Number.between(1, 5) }
+    ideal_quantity { Faker::Number.between(from: 1, to: 10) }
+    current_quantity { Faker::Number.between(from: 1, to: 5) }
   end
 end
