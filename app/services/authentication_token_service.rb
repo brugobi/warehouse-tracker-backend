9055d9219d1d0ca9783911bd555870f4a0a04392
brugobi <brgobbi@hotmail.com>
2021-06-15 16:23:58 -0400

    add JWT authentication but the test is failing

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,5 +1,10 @@
 class AuthenticationTokenService
+  HMAC_SECRET = 'my$ecretK3y'.freeze
+  ALGORITHM_TYPE = 'HS256'.freeze
+
   def self.call
-    '123'
+    payload = { 'test' => 'blah' }
+
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 describe AuthenticationTokenService do
   describe '.call' do
+    let(:token) { described_class.call }
+
     it 'returns an authentication token' do
-      expect(described_class.call).to eq('123')
+      decoded_token = JWT.decode(token,
+                                 described_class::HMAC_SECRET,
+                                 true,
+                                 { algorithm: described_class::ALGORITHM_TYPE })
+
+      expect(decoded_token.call).to eq(
+        [
+          { 'test' => 'blah' },
+          { 'alg' => 'HS256' }
+        ]
+      )
     end
   end
 end
