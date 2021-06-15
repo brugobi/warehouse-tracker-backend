9c8ec430f30a8c01553440327926502f94246a22
brugobi <brgobbi@hotmail.com>
2021-06-15 14:56:16 -0400

    add authentication token service file and test it

---
+++ app/services/authentication_token_service.rb
@@ -0,0 +1,3 @@
+class AuthenticationTokenService
+
+end
---
+++ spec/services/authentication_token_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe AuthenticationTokenService do
+  describe '.call' do
+    it 'returns an authentication token' do
+      
+    end
+  end
+end
