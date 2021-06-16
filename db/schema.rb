6c4ed1b6a1c1e55d2851f7593b78d138d37ada42
brugobi <brgobbi@hotmail.com>
2021-06-16 11:20:07 -0400

    add password_digest column to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_202720) do
+ActiveRecord::Schema.define(version: 2021_06_16_150023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_202720) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
