f41fb9ac11210c764d6f7a5e262cf2dc19724d08
brugobi <brgobbi@hotmail.com>
2021-06-16 12:11:00 -0400

    fix a user to @user

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -17,7 +17,7 @@ module Api
       private
 
       def user
-        user ||= User.find_by(username: params.require(:username))
+        @user ||= User.find_by(username: params.require(:username))
       end
 
       def parameter_missing(err)
