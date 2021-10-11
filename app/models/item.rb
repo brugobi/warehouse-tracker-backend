06df52600b7c58a4945007e874e22561ff0259fd
brugobi <brgobbi@hotmail.com>
2021-10-11 16:16:07 -0400

    refactoring validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :code, :ideal_quantity, :current_quantity, presence: true
-  validates_uniqueness_of :code, case_sensitive: false
   belongs_to :user
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :ideal_quantity, :current_quantity, presence: true
+  validates :code, uniqueness: { case_sensitive: false }, presence: true
 end
